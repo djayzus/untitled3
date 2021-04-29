af5dd08340d6ded8ac6588bf7ea867e675e7d21c
djayzus <79160224+djayzus@users.noreply.github.com>
2021-04-29 13:39:51 -0400

    Initial commit

---
+++ Menu.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+namespace menu {
+
+    void menuMain() {
+
+        std::cout << "Fait un choix parmis les options suivantes\n" << std::endl;
+        std::cout << " 0. Quitter" << std::endl;
+        std::cout << " 1. Ajouter un cout de location avec ses informations" << std::endl;
+        std::cout << " 2. Ajouter un cout en electricite avec ses informations" << std::endl;
+        std::cout << " 3. Ajouter un cout pour la consommation d'eau avec ses informations" << std::endl;
+        std::cout << " 4. Afficher un compte-rendu de la machine avec les couts entres, ainsi que les couts totaux\n"
+                     "    d'utilisation de la machine pour une periode determinee par la personne utilisatrice." << std::endl;
+
+    }
+
+}
+
+namespace calcul {
+
+    void afficherCalcul() {
+        int jour = 0;
+        std::cout << "Entrez nombre de jours a utiliser pour le calcul" << std::endl;
+        std::cin >> jour;
+
+    }
+
+
+}
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include "eau.h"
+#include "Menu.h"
+
+int main() {
+
+    int choix = 0;
+    bool fin = false;
+
+    while (!fin) {
+        menu::menuMain();
+        std::cout << "\nFaites votre choix: "<< std::endl;
+        std::cin >> choix;
+        switch (choix) {
+
+            case 0:
+                std::cout << "Fin de l'application!"<< std::endl;
+                fin = true;
+                break;
+            case 1:
+                //Ajouter un coût de location avec ses informations
+                std::cout << "allo1\n";
+                break;
+            case 2:
+                //Ajouter un coût en électricité avec ses informations
+                std::cout << "allo2\n";
+                break;
+            case 3:
+                //Ajouter un coût pour la consommation d'eau avec ses informations
+                std::cout << "allo3\n";
+                break;
+            case 4:
+                //Afficher un compte-rendu de la machine avec les coûts entrés, ainsi que les coûts totaux
+                //d'utilisation de la machine pour une période déterminée par la personne utilisatrice
+                calcul::afficherCalcul();
+                break;
+            default:
+                std::cout << "Choix invalide" << std::endl;
+                break;
+        }
+
+    }
+}
